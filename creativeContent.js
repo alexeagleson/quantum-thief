aab2b8d3b9ecf2a3c893925bfe95b01a72aa76bc
Glitch (curved-keyboard) <none>
2018-02-25 11:00:30 +0000

    ☂️💣 Checkpoint creativeContent.js:702057/487

--- creativeContent.js
+++ creativeContent.js
@@ -87,8 +87,8 @@ var menuResponse = {
     setTimeout(function() { showMenu(creativeContent.retiredRobocop2Dialogue2); }, 10);
     return false;
   },
-  throneDialogue3: function() {
-    setTimeout(function() { showMenu(creativeContent.throneDialogue3); }, 10);
+  retiredRobocop2Dialogue3: function() {
+    setTimeout(function() { showMenu(creativeContent.retiredRobocop2Dialogue3); }, 10);
     return false;
   },
   
@@ -295,6 +295,9 @@ var creativeContent = {
     emptyCell = oldPersonUniverse[0].randomEmptyCellCoords();
     oldPersonUniverse[0].addObjectToMap(new Object(char = "*", name = "Robocop - Retired", wall = false, alive = true, clickFunction = "talk", myDialogue = creativeContent.retiredRobocopDialogue, portraitChar = "-", moveType = "follow"), emptyCell.x, emptyCell.y);
 
+    emptyCell = oldPersonUniverse[0].randomEmptyCellCoords();
+    oldPersonUniverse[0].addObjectToMap(new Object(char = "$", name = "Robocop - Finally Alive", wall = false, alive = true, clickFunction = "talk", myDialogue = creativeContent.retiredRobocop2Dialogue, portraitChar = "-", moveType = "follow"), emptyCell.x, emptyCell.y);
+
   return oldPersonUniverse;
   },
   
@@ -457,21 +460,21 @@ var creativeContent = {
   },
   retiredRobocopDialogue2: {
       textStrings: ["Robocop - Retired", "Well sure, I'm just a retired cop looking for the REAL Good Stuff. Not the Regular Good Ones. You know the ones I mean.", "...", "Ah that sweet mess. Where have my robotic dentures got to now?"],
-      spaces: [0, 1, 0, 1],
+      spaces: [0, 1, 0],
       fgColours: ["red", "lightyellow", "#b0ec62", "lightyellow"],
       responseFunction: {}
   },
   retiredRobocop2Dialogue: {
       textStrings: ["Robocop - Finally Alive", "Hello what are you doing here?", "{I'm looking for booty. Do you know where I can find something to impress my boss?}"],
       spaces: [0, 1, 0],
-      fgColours: ["red", "lightyellow", "b0ec62", "lightyellow"],
+      fgColours: ["red", "lightyellow", "#b0ec62", "lightyellow"],
       responseFunction: {"{I'm looking for booty. Do you know where I can find something to impress my boss?}": menuResponse.retiredRobocop2Dialogue2}
   },
   retiredRobocop2Dialogue2: {
       textStrings: ["Robocop - Finally Alive", "Get your hands on a crinkly brown bag and you'll always be impressive to me."],
       spaces: [0, 1],
       fgColours: ["red", "lightyellow"],
-      responseFunction: {"there is not much more to tell": menuResponse.throneDialogue3}
+      responseFunction: {"there is not much more to tell": menuResponse.retiredRobocop2Dialogue3}
   }
 }
 
